62dff5ffd8a0702343b35bdaf08cbd9568f30028
girish1993 <girishbhatta93@gmail.com>
2025-07-04 10:22:49 +1000

    projection task started

---
+++ nano/exp/projection.py
@@ -0,0 +1,22 @@
+
+#%%
+import numpy as np
+
+
+def apply_dot(arr1, arr2):
+    return np.dot(arr1, arr2)
+
+rand_3d_matrix = np.random.rand(100, 3)
+basis_vectors = np.array([
+                    [1, 0],
+                    [0, 1],
+                    [0, 0]
+                ])
+
+
+print(rand_3d_matrix)
+
+
+
+
+# %%
