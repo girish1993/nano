77c276d5543c3b0fb68729c851005547ea351667
girish1993 <girishbhatta93@gmail.com>
2025-07-08 12:03:59 +1000

    gram schmidt process implemented for basis vectors

--- nano/exp/projection.py
+++ nano/exp/projection.py
@@ -9,9 +9,9 @@ sys.path.append(dir_path)
 parent_dir = os.path.dirname(dir_path)
 sys.path.append(parent_dir)
 
-import numpy as np  # noqa: E402
-import matplotlib.pyplot as plt  # noqa: E402
 import matplotlib.cm as cm  # noqa: E402
+import matplotlib.pyplot as plt  # noqa: E402
+import numpy as np  # noqa: E402
 from base.matrix import Matrix  # noqa: E402
 from base.vector import Vector  # noqa: E402
 
@@ -44,11 +44,18 @@ ax = fig.add_subplot(111, projection="3d")
 colors = np.random.rand(rand_3d_matrix.shape[0])
 
 cmap = cm.get_cmap("Paired")
-colors = cmap(np.linspace(0,1,rand_3d_matrix.shape[0]))
+colors = cmap(np.linspace(0, 1, rand_3d_matrix.shape[0]))
 
 for i in range(len(rand_3d_matrix)):
-    ax.quiver(0, 0, 0, rand_3d_matrix[i, 0], rand_3d_matrix[i, 1], rand_3d_matrix[i, 2],
-              color=colors[i])
+    ax.quiver(
+        0,
+        0,
+        0,
+        rand_3d_matrix[i, 0],
+        rand_3d_matrix[i, 1],
+        rand_3d_matrix[i, 2],
+        color=colors[i],
+    )
 
 ax.set_xlabel("X Axis")
 ax.set_ylabel("Y Axis")
@@ -61,20 +68,32 @@ ax.set_zlim([0, np.max(rand_3d_matrix[:, 2])])
 plt.show()
 
 
+# %%
 
-#%%
+r_basis = np.random.rand(3, 2)
 
-r_basis = np.random.rand(3,2)
 
-def _check_for_independence(X:np.ndarray) -> bool:
-    return all(np.cross(X[:, 0], X[:, 1]) == np.zeros(3)) 
+def _check_for_independence(X: np.ndarray) -> bool:
+    return all(np.cross(X[:, 0], X[:, 1]) != np.zeros(3))
+
 
 def _orthonormal_basis(X: np.ndarray) -> np.ndarray:
-    
+    # this is just for (3,2)
+
+    if _check_for_independence:
+        u1 = r_basis[:, 0] / np.linalg.norm(r_basis[:, 0])
+        u2 = r_basis[:, 1] - ((np.dot(r_basis[:, 1], u1)) * u1)
+
+        u2 = u2 / np.linalg.norm(u2)
+        return np.array([u1, u2]).T
+
+    raise ValueError("Cannot orthonormalise dependent vectors")
 
 
+basis = _orthonormal_basis(X=r_basis)
 
-#%%
+print("check for orthogonality of basis vectors")
+print(basis)
+print("dot product of basis vectors", round(np.dot(basis[:, 0], basis[:, 1])))
 
-
-
+# %%
